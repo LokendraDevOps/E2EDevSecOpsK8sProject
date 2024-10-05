ccc1b079848cd167cbac2979cd61300942f11196
LokendraDevOps <lokendraprataprock@gmail.com>
2024-10-05 17:57:03 +0530

    Second Commit

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "test-project-backet1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    key            = "End-to-End DevSecOps Kubernetes Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "My-DynamoDB-Table"
     encrypt        = true
   }
   required_version = ">=0.13.0"
